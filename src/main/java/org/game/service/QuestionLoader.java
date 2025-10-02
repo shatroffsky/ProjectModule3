70dce1d5b2a73ab30b051049260b60bcfe462909
Vladyslav Shatrovskyi <shatroffsky@gmail.com>
2025-10-02 18:17:25 +0300

    Add logger to QuestionLoader.

--- src/main/java/org/game/service/QuestionLoader.java
+++ src/main/java/org/game/service/QuestionLoader.java
@@ -2,6 +2,8 @@ package org.game.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.game.model.Question;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContext;
 import java.io.InputStream;
@@ -9,19 +11,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class QuestionLoader {
+    private static final Logger log = LoggerFactory.getLogger(QuestionLoader.class);
+
     private static final String QUESTIONS_FILE = "/WEB-INF/questions.json";
 
     public static Map<Integer, Question> loadQuestions(ServletContext context) throws Exception {
+        log.info("Loading questions from {}", QUESTIONS_FILE);
+
         try (InputStream is = context.getResourceAsStream(QUESTIONS_FILE)) {
+            if (is == null) {
+                log.error("Questions file '{}' not found in servlet context", QUESTIONS_FILE);
+                throw new IllegalStateException("Questions file not found: " + QUESTIONS_FILE);
+            }
+
             ObjectMapper mapper = new ObjectMapper();
             Question[] questions = mapper.readValue(is, Question[].class);
 
             Map<Integer, Question> questionMap = new HashMap<>();
             for (Question q : questions) {
                 questionMap.put(q.getStep(), q);
+                log.debug("Loaded question step={} text='{}'", q.getStep(), q.getText());
             }
+
+            log.info("Successfully loaded {} questions", questionMap.size());
             return questionMap;
+        } catch (Exception e) {
+            log.error("Failed to load questions from {}", QUESTIONS_FILE, e);
+            throw e;
         }
     }
 }
-
